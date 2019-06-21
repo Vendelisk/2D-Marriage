adc9d11f779c33037a90e2fb7a0c0eff56fcd986
Vendelisk <jkohn.bus@gmail.com>
2019-06-21 08:38:51 -0700

    Removed a LOT of stuff I wasn't using.

---
+++ Assets/Scripts/AudioChecker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+ 
+public class AudioChecker : MonoBehaviour {
+ 
+	AudioSource[] sources;
+             
+    void Start () {      
+		//Get every single audio sources in the scene.
+		sources = GameObject.FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+		foreach(AudioSource audioSource in sources) {
+			if(audioSource.transform.parent != null) {
+				if(audioSource.transform.parent.parent != null) {
+					if(audioSource.transform.parent.parent.parent != null) {
+						if(audioSource.transform.parent.parent.parent.parent != null) {
+							Debug.Log(audioSource.transform.parent.parent.parent.parent.name+" has "+audioSource.clip.name+ " attached to it");
+							continue;
+						}
+						Debug.Log(audioSource.transform.parent.parent.parent.name+" has "+audioSource.clip.name+ " attached to it");
+						continue;
+					}
+					Debug.Log(audioSource.transform.parent.parent.name+" has "+audioSource.clip.name+ " attached to it");
+					continue;
+				}
+				Debug.Log(audioSource.transform.parent.name+" has "+audioSource.clip.name+ " attached to it");
+				continue;
+			}
+			Debug.Log(audioSource.transform.name+" has "+audioSource.clip.name+ " attached to it");
+		}
+	}
+}
